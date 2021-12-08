a52606d554ba54ac73bbd331b5a457e6d5c6dd93
Filip Wicher <filip.wicher@allegro.pl>
2021-12-08 09:54:20 +0100

    no more ponglish

--- src/main/kotlin/adventOfCode/year2021/Day8.kt
+++ src/main/kotlin/adventOfCode/year2021/Day8.kt
@@ -22,37 +22,37 @@ class Day8: AdventOfCodeDay {
             val left = leftAndRight.first().split(" ")
             val right = leftAndRight.last().split(" ")
 
-            val jedynka = left.single { it.length == 2 }
-            val czworka = left.single { it.length == 4 }
-            val siodemka = left.single { it.length == 3}
-            val osemka = left.single { it.length == 7}
-            val szostka = left.single { it.length == 6 && it.count { char -> jedynka.contains(char) }==1 }
-            val zero = left.single { it.length == 6 && !it.toList().containsAll(szostka.toList()) && !it.toList().containsAll(czworka.toList())}
-            val dziewiatka = left.single { it.length == 6 && !it.toList().containsAll(szostka.toList()) && !it.toList().containsAll(zero.toList())}
-            val prawaGorna = left.single { it.length == 7 }.filterNot { szostka.contains(it) }
-            val piatka = left.single { it.length == 5 && !it.contains(prawaGorna) }
-            val lewaDolna = left.single { it.length == 7 }.filterNot { dziewiatka.contains(it) }
-            val trojka = left.single { it.length == 5 && !it.toList().containsAll(piatka.toList()) && !it.contains(lewaDolna) }
-            val dwojka = left.single { it.length == 5 && !it.toList().containsAll(piatka.toList()) && !it.toList().containsAll(trojka.toList()) }
+            val one = left.single { it.length == 2 }
+            val four = left.single { it.length == 4 }
+            val seven = left.single { it.length == 3}
+            val eight = left.single { it.length == 7}
+            val six = left.single { it.length == 6 && it.count { char -> one.contains(char) }==1 }
+            val zero = left.single { it.length == 6 && !it.toList().containsAll(six.toList()) && !it.toList().containsAll(four.toList())}
+            val nine = left.single { it.length == 6 && !it.toList().containsAll(six.toList()) && !it.toList().containsAll(zero.toList())}
+            val topRight = left.single { it.length == 7 }.filterNot { six.contains(it) }
+            val five = left.single { it.length == 5 && !it.contains(topRight) }
+            val bottomLeft = left.single { it.length == 7 }.filterNot { nine.contains(it) }
+            val three = left.single { it.length == 5 && !it.toList().containsAll(five.toList()) && !it.contains(bottomLeft) }
+            val two = left.single { it.length == 5 && !it.toList().containsAll(five.toList()) && !it.toList().containsAll(three.toList()) }
 
             val mapOfNumbers = mapOf(
                 zero to "0",
-                jedynka to "1",
-                dwojka to "2",
-                trojka to "3",
-                czworka to "4",
-                piatka to "5",
-                szostka to "6",
-                siodemka to "7",
-                osemka to "8",
-                dziewiatka to "9"
+                one to "1",
+                two to "2",
+                three to "3",
+                four to "4",
+                five to "5",
+                six to "6",
+                seven to "7",
+                eight to "8",
+                nine to "9"
             )
 
-            var liczba = ""
+            var number = ""
             right.map {
-                liczba += mapOfNumbers[mapOfNumbers.keys.find { key -> key.toList().containsAll(it.toList()) && key.length == it.length }]
+                number += mapOfNumbers[mapOfNumbers.keys.find { key -> key.toList().containsAll(it.toList()) && key.length == it.length }]
             }
-            sum += liczba.toInt()
+            sum += number.toInt()
         }
 
         return sum
